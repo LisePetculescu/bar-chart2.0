f7628182eeebca4fa501f8eed2fa3d79ae070676
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-04-12 09:45:32 +0200

    can take one height from array and put on one bar

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 
 window.addEventListener("load", start);
 
+const data = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 2,
+  3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+
 function start() {
   console.log("js is running...");
 
   const bars = document.querySelectorAll(".bar");
   const firstBar = bars[0];
 
-  let height = 40;
+  let height = (data[0] / 32) * 100;
 
   firstBar.style.height = `${height}px`;
 }
