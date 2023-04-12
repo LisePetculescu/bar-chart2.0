ed29f78e70f6efe4055a0c89084ff5b4fc3c3295
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-04-12 09:31:40 +0200

    added height to the first bar

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-"use strict"; 
+"use strict";
 
+window.addEventListener("load", start);
+
+function start() {
+  console.log("js is running...");
+
+  const bars = document.querySelectorAll(".bar");
+  const firstBar = bars[0];
+
+  let height = 40;
+
+  firstBar.style.height = `${height}px`;
+}
